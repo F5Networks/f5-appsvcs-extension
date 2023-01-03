c5a159ae3f613eee3a0250cda400c8cbbde20b5f
Mike Levine <m.levine@f5.com>
2023-01-03 13:04:08 -0800

    Update copyright

--- src/lib/tracer.js
+++ src/lib/tracer.js
@@ -1,6 +1,5 @@
-/* eslint-disable max-classes-per-file */
 /**
- * Copyright 2022 F5 Networks, Inc.
+ * Copyright 2023 F5 Networks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/* eslint-disable max-classes-per-file */
 
 'use strict';
 
